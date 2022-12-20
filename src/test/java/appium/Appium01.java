099b6720c760c0bad6850ba400dcb5d18313753f
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-12-20 11:04:33 +0300

    20122022

---
+++ src/test/java/appium/Appium01.java
@@ -0,0 +1,25 @@
+package appium;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium01 {
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Cuneyt");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Ramazan\\IdeaProjects\\Appium\\src\\Apps\\gestureTool.apk");
+        capabilities.setCapability("appPackage","com.davemac327.gesture.tool");// KOnsola adbs shell enter yapıp "dumpsys window | grep -E "mCurrentFocus" enter"
+        capabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
+        AndroidDriver<MobileElement>driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+    }
+}
